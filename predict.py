15e1f5bedf29eb7b3f0ee0c342098a10272bdc86
QuocThinhNguyen <nguyenquocthinh111@gmail.com>
2025-04-25 12:19:06 +0700

    update

--- predict.py
+++ predict.py
@@ -5,8 +5,11 @@ import pandas as pd
 import pickle
 from surprise import Reader, Dataset
 from utils import preprocess_data
+import os
 from flask_cors import cross_origin
 import requests
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -15,39 +18,25 @@ with open("svd_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 @app.route("/recommend", methods=["POST"])
-@cross_origin(origins=["http://localhost:5173"], supports_credentials=True)
+@cross_origin(origins=[os.environ.get("FRONTEND_URL")], supports_credentials=True)
 def recommend():
     patient_id = request.json.get("patient_id")
-    # print("Patient ID:", patient_id)
-    # data = request.json.get("data")
 
-    url = "http://localhost:9000/user/suggestions?limit=100"
+    BASE_API_URL = os.environ.get("BACKEND_URL")
+    print("BASE_API_URL:", BASE_API_URL)
+
+    url = f"{BASE_API_URL}/user/suggestions?limit=100"
     response = requests.get(url)
     if response:
         print("Data fetched successfully from the external service.")
     else:
         print("Failed to fetch data from the external service.")
 
-
     data = response.json()['data']
 
     print(data) 
 
-#     data = [
-#     {'patient_id': 1, 'doctor_id': 101, 'specialty_id': 1, 'visits': 3, 'rating': 4.5, 'last_visit_date': '2025-04-11', 'click_count': 20},
-#     {'patient_id': 1, 'doctor_id': 102, 'specialty_id': 2, 'visits': 1, 'rating': 4.0, 'last_visit_date': '2025-04-11', 'click_count': 40},
-#     {'patient_id': 2, 'doctor_id': 105, 'specialty_id': 1, 'visits': 2, 'rating': 5.0, 'last_visit_date': '2025-01-20', 'click_count': 1},
-#     {'patient_id': 3, 'doctor_id': 103, 'specialty_id': 2, 'visits': 4, 'rating': 4.8, 'last_visit_date': '2025-03-24', 'click_count': 7},
-#     {'patient_id': 3, 'doctor_id': 104, 'specialty_id': 3, 'visits': 1, 'rating': 4.8, 'last_visit_date': '2025-03-25', 'click_count': 10},
-#     {'patient_id': 4, 'doctor_id': 106, 'specialty_id': 2, 'visits': 0, 'rating': 5.0, 'last_visit_date': None, 'click_count': 1},
-#     {'patient_id': 4, 'doctor_id': 107, 'specialty_id': 3, 'visits': 0, 'rating': 4.9, 'last_visit_date': None, 'click_count': 10},
-# ]
-
     df = preprocess_data(data)
-    # df['last_visit_date'] = pd.to_datetime(df['last_visit_date'], errors='coerce')
-
-    print("Data after preprocessing:")
-    print(df.head())
 
     all_doctor_ids = df['doctor_id'].unique().tolist()
     known_doctor_ids = df[df['patient_id'] == patient_id]['doctor_id'].tolist()
@@ -71,12 +60,6 @@ def recommend():
         df['last_visit_date'] = pd.to_datetime(df['last_visit_date'], errors='coerce')
         recent_date = df[df['doctor_id'] == doc_id]['last_visit_date'].max()
         if pd.notnull(recent_date):
-            # recent_date = recent_date.split('T')[0]
-            # recent_date = str(recent_date).split('T')[0]  # Đảm bảo recent_date là chuỗi
-            # recent_date = datetime.strptime(recent_date, '%Y-%m-%d')
-            # days_ago = (datetime.now() - recent_date).days
-            # recent_bonus = max(0, (30 - days_ago) / 30) * 0.2
-            # bonus += recent_bonus
             recent_date = recent_date.tz_localize(None) 
             days_ago = (datetime.now() - recent_date).days
             recent_bonus = max(0, (30 - days_ago) / 30) * 0.2
